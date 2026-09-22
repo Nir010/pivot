22102c5e62094f1dede930b847b9099b7b9b594a
Nir010 <nirajadhikari7890@gmail.com>
2026-09-22 16:33:33 +0545

    more info button on team

--- js/main.js
+++ js/main.js
@@ -188,6 +188,7 @@ document.addEventListener('DOMContentLoaded', () => {
               <line x1="12" y1="16" x2="12" y2="12"></line>
               <line x1="12" y1="8" x2="12.01" y2="8"></line>
             </svg>
+            <span class="team-info-label">More info</span>
           </button>
         </div>
       </div>`;

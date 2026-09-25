5c92af57189a51691ce6116006cb2a996b297ffc
Nir010 <nirajadhikari7890@gmail.com>
2026-09-25 22:00:44 +0545

    jazzmin added

--- pivot/settings.py
+++ pivot/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',          # admin theme
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
